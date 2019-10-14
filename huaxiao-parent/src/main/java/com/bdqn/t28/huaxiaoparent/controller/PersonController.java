dd68a550855d44627545c305fc2452874ba70ff6
xiaocaiqq <51457584+xiaocaiqq@users.noreply.github.com>
2019-10-14 20:28:32 +0800

    基本架构

---
+++ huaxiao-parent/src/main/java/com/bdqn/t28/huaxiaoparent/controller/PersonController.java
@@ -0,0 +1,4 @@
+package com.bdqn.t28.huaxiaoparent.controller;
+
+public class PersonController {
+}
---
+++ huaxiao-parent/src/main/java/com/bdqn/t28/huaxiaoparent/vo/PageResult.java
@@ -0,0 +1,51 @@
+package com.bdqn.t28.huaxiaoparent.vo;
+
+import java.util.List;
+
+/**
+ * 分页view
+ * @param <T>
+ */
+public class PageResult<T> {
+
+    private Long total; //总条数
+    private Integer totalPage; //总页数
+    private List<T> items; //当前页数据
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void setItems(List<T> items) {
+        this.items = items;
+    }
+
+    public PageResult() {}
+
+    public PageResult(Long total, List<T> items) {
+        this.total = total;
+        this.items = items;
+    }
+
+    public PageResult(Long total, Integer totalPage, List<T> items) {
+        this.total = total;
+        this.totalPage = totalPage;
+        this.items = items;
+    }
+}
